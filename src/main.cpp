d7cb0776a06bbd9adaf94d3636a2e5a4b31594cf
Muhamad Bekti Wibowo <bowo1093334@gmail,com>
2025-07-04 05:07:41 +0700

    basic display mini program

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,16 @@
-
 /*******************************************************************************
- * LVGL Widgets
- * This is a widgets demo for LVGL - Light and Versatile Graphics Library
- * import from: https://github.com/lvgl/lv_demos.git
- *
- * Dependent libraries:
- * LVGL: https://github.com/lvgl/lvgl.git
-
- * Touch libraries:
- * FT6X36: https://github.com/strange-v/FT6X36.git
- * GT911: https://github.com/TAMCTec/gt911-arduino.git
- * XPT2046: https://github.com/PaulStoffregen/XPT2046_Touchscreen.git
- *
- * LVGL Configuration file:
- * Copy your_arduino_path/libraries/lvgl/lv_conf_template.h
- * to your_arduino_path/libraries/lv_conf.h
- * Then find and set:
- * #define LV_COLOR_DEPTH     16
- * #define LV_TICK_CUSTOM     1
- *
- * For SPI display set color swap can be faster, parallel screen don't set!
- * #define LV_COLOR_16_SWAP   1
- *
- * Optional: Show CPU usage and FPS count
- * #define LV_USE_PERF_MONITOR 1
+ * LVGL Simple Widgets Demo
+ * Simplified version for ESP32 with RGB display and GT911 touch
  ******************************************************************************/
-//#include "lv_demo_widgets.h"
+
 #include <lvgl.h>
 #include <demos/lv_demos.h>
-/*******************************************************************************
- ******************************************************************************/
 #include <Arduino_GFX_Library.h>
+
+// Display backlight pin
 #define TFT_BL 2
-#define GFX_BL DF_GFX_BL // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
 
-/* More dev device declaration: https://github.com/moononournation/Arduino_GFX/wiki/Dev-Device-Declaration */
-#if defined(DISPLAY_DEV_KIT)
-Arduino_GFX *gfx = create_default_Arduino_GFX();
-#else /* !defined(DISPLAY_DEV_KIT) */
-
-/* More data bus class: https://github.com/moononournation/Arduino_GFX/wiki/Data-Bus-Class */
-//Arduino_DataBus *bus = create_default_Arduino_DataBus();
-
-/* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
-//Arduino_GFX *gfx = new Arduino_ILI9341(bus, DF_GFX_RST, 0 /* rotation */, false /* IPS */);
-
+// RGB Display configuration
 Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
     GFX_NOT_DEFINED /* CS */, GFX_NOT_DEFINED /* SCK */, GFX_NOT_DEFINED /* SDA */,
     40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
@@ -52,39 +18,38 @@ Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
     5 /* G0 */, 6 /* G1 */, 7 /* G2 */, 15 /* G3 */, 16 /* G4 */, 4 /* G5 */,
     8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */
 );
-// option 1:
+
 // ILI6485 LCD 480x272
 Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
-  bus,
-  480 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 43 /* hsync_back_porch */,
-  272 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 12 /* vsync_back_porch */,
-  1 /* pclk_active_neg */, 9000000 /* prefer_speed */, true /* auto_flush */);
+    bus,
+    480 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 43 /* hsync_back_porch */,
+    272 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 12 /* vsync_back_porch */,
+    1 /* pclk_active_neg */, 9000000 /* prefer_speed */, true /* auto_flush */);
 
- // option 2:
- // ST7262 IPS LCD 800x480
-// Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
-//   bus,
-//   800 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 8 /* hsync_back_porch */,
-//   480 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 8 /* vsync_back_porch */,
-//   1 /* pclk_active_neg */, 14000000 /* prefer_speed */, true /* auto_flush */);
-#endif /* !defined(DISPLAY_DEV_KIT) */
-/*******************************************************************************
- * End of Arduino_GFX setting
- ******************************************************************************/
+// GT911 Touch configuration
+#include <Wire.h>
+#include <TAMC_GT911.h>
 
-/*******************************************************************************
- * Please config the touch panel in touch.h
- ******************************************************************************/
-#include "touch.h"
+#define TOUCH_GT911_SCL 20
+#define TOUCH_GT911_SDA 19
+#define TOUCH_GT911_INT 18
+#define TOUCH_GT911_RST 17
+#define TOUCH_MAP_X1 480
+#define TOUCH_MAP_X2 0
+#define TOUCH_MAP_Y1 272
+#define TOUCH_MAP_Y2 0
 
-/* Change to your screen resolution */
-static uint32_t screenWidth;
-static uint32_t screenHeight;
+TAMC_GT911 ts = TAMC_GT911(TOUCH_GT911_SDA, TOUCH_GT911_SCL, TOUCH_GT911_INT, TOUCH_GT911_RST,
+                           max(TOUCH_MAP_X1, TOUCH_MAP_X2), max(TOUCH_MAP_Y1, TOUCH_MAP_Y2));
+
+int touch_last_x = 0, touch_last_y = 0;
+
+// LVGL display and input
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t *disp_draw_buf;
 static lv_disp_drv_t disp_drv;
 
-/* Display flushing */
+// Display flush callback
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
 {
   uint32_t w = (area->x2 - area->x1 + 1);
@@ -99,22 +64,15 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
   lv_disp_flush_ready(disp);
 }
 
+// Touch input callback
 void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 {
-  if (touch_has_signal())
+  ts.read();
+  if (ts.isTouched)
   {
-    if (touch_touched())
-    {
-      data->state = LV_INDEV_STATE_PR;
-
-      /*Set the coordinates*/
-      data->point.x = touch_last_x;
-      data->point.y = touch_last_y;
-    }
-    else if (touch_released())
-    {
-      data->state = LV_INDEV_STATE_REL;
-    }
+    data->state = LV_INDEV_STATE_PR;
+    data->point.x = map(ts.points[0].x, TOUCH_MAP_X1, TOUCH_MAP_X2, 0, gfx->width() - 1);
+    data->point.y = map(ts.points[0].y, TOUCH_MAP_Y1, TOUCH_MAP_Y2, 0, gfx->height() - 1);
   }
   else
   {
@@ -125,18 +83,16 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 void setup()
 {
   Serial.begin(115200);
-  // while (!Serial);
-  Serial.println("LVGL Widgets Demo");
+  Serial.println("LVGL Simple Demo Starting...");
 
-  // Init touch device
-  
-
-  // Init Display
+  // Initialize display
   gfx->begin();
-#ifdef TFT_BL
+
+  // Setup backlight
   pinMode(TFT_BL, OUTPUT);
   digitalWrite(TFT_BL, HIGH);
-#endif
+
+  // Test display with color fills
   gfx->fillScreen(RED);
   delay(500);
   gfx->fillScreen(GREEN);
@@ -144,51 +100,57 @@ void setup()
   gfx->fillScreen(BLUE);
   delay(500);
   gfx->fillScreen(BLACK);
-  delay(500);
+
+  // Initialize LVGL
   lv_init();
-  delay(10);
-  touch_init();
-  screenWidth = gfx->width();
-  screenHeight = gfx->height();
-#ifdef ESP32
-  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight/4 , MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
-#else
-  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * screenHeight/4);
-#endif
+
+  // Initialize touch
+  Wire.begin(TOUCH_GT911_SDA, TOUCH_GT911_SCL);
+  ts.begin();
+  ts.setRotation(ROTATION_NORMAL);
+
+  // Setup display buffer
+  uint32_t screenWidth = gfx->width();
+  uint32_t screenHeight = gfx->height();
+
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 4,
+                                                 MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+
+  Serial.printf("Free heap: %u bytes\n", ESP.getFreeHeap());
+  Serial.printf("Free PSRAM: %u bytes\n", ESP.getFreePsram());
+  
   if (!disp_draw_buf)
   {
-     Serial.printf("Free heap: %u bytes\n", ESP.getFreeHeap());
-    Serial.printf("Free PSRAM: %u bytes\n", ESP.getFreePsram());
-    Serial.println("LVGL disp_draw_buf allocate failed!");
+    Serial.println("LVGL buffer allocation failed!");
+
+    while (1)
+      ; // Stop execution
   }
-  else
-  {
-    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight/4);
 
-    /* Initialize the display */
-    lv_disp_drv_init(&disp_drv);
-    /* Change the following line to your display resolution */
-    disp_drv.hor_res = screenWidth;
-    disp_drv.ver_res = screenHeight;
-    disp_drv.flush_cb = my_disp_flush;
-    disp_drv.draw_buf = &draw_buf;
-    lv_disp_drv_register(&disp_drv);
+  // Initialize display driver
+  lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight / 4);
+  lv_disp_drv_init(&disp_drv);
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
 
-    /* Initialize the (dummy) input device driver */
-    static lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = my_touchpad_read;
-    lv_indev_drv_register(&indev_drv);
+  // Initialize input driver
+  static lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  indev_drv.read_cb = my_touchpad_read;
+  lv_indev_drv_register(&indev_drv);
 
-    lv_demo_widgets();
+  // Start demo
+  lv_demo_widgets();
 
-    Serial.println("Setup done");
-  }
+  Serial.println("Setup completed successfully!");
 }
 
 void loop()
 {
-  lv_timer_handler(); /* let the GUI do its work */
+  lv_timer_handler(); // Handle LVGL tasks
   delay(5);
 }
